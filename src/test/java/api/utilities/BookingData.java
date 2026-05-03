7a4877f58a1ab737a1f43bcee6d27e34a0148e93
Aniket <karanjekaraniket@gmail.com>
2026-05-03 15:10:32 +0530

    modifield file name

--- src/test/java/api/utilities/BookingData.java
+++ src/test/java/api/utilities/BookingData.java
@@ -9,6 +9,6 @@ public class BookingData
 
         String path = System.getProperty("user.dir") + "/src/test/resources/BookingData.xlsx";
 
-        return ExcelUtils.getExcelData("BookingData.xlsx", "Sheet1");
+        return ExcelUtils.getExcelData("bookingdata.xlsx", "Sheet1");
     }
 }
